75a3b5524dcf702926c88819e0adb6f9503b8c87
Keila Cristina <keilacris2001clr@gmail.com>
2023-11-03 00:21:27 -0300

    pegar e gravar dados da autenticação ok

--- js/auth.js
+++ js/auth.js
@@ -8,15 +8,17 @@ firebase.initializeApp({
             messagingSenderId: "252926034214",
             appId: "1:252926034214:web:f07523455ac9e16ed641e7"
   });
-
+  
+  // Iniciar Banco de dados Firestone
   var db = firebase.firestore();
 
         const cadastroForm = document.getElementById('cadastro-form');
         const cadastroCpf = document.getElementById('cadastro-cpf');
+        const cadastroNome = document.getElementById('cadastro-nome');
         const cadastroTelefone = document.getElementById('cadastro-telefone');
         const cadastroEmail = document.getElementById('cadastro-email');
         const cadastroSenha = document.getElementById('cadastro-senha');
-        const signupButton = document.getElementById('signup-button');
+        const cadastroBotao = document.getElementById('cadastro-botao');
 
         const loginForm = document.getElementById('login-form');
         const loginEmail = document.getElementById('login-email');
@@ -26,38 +28,39 @@ firebase.initializeApp({
         const message = document.getElementById('message');
 
         // Função para lidar com o cadastro
-        signupButton.addEventListener('click', () => {
+        cadastroBotao.addEventListener('click', () => {
             const email = cadastroEmail.value;
-            const password = cadastroSenha.value;
+            const senha = cadastroSenha.value;
+            const nome = cadastroNome.value;
+            const cpf = cadastroCpf.value;
+            const telefone = cadastroTelefone.value;
+           
 
-            firebase.auth().createUserWithEmailAndPassword(email, password)
+            firebase.auth().createUserWithEmailAndPassword(email, senha)
                 .then(userCredential => {
-                    const user = userCredential.user;
+                const user = userCredential.user;
 
-                    db.collection("users").add({
-                        cpf: cadastroCpf,
-                        telefone: cadastroTelefone
+                    db.collection("users").doc(cpf).set({
+                        email: email,
+                        nome: nome,
+                        cpf: cpf,
+                        telefone: telefone,
+                        permissao: "usuario"
                       })
-                      .then(() => {
-                        message.textContent = 'Cadastro realizado com sucesso';
-                      })
-                      .catch((error) => {
-                        console.error("Erro ao armazenar informações adicionais: ", error);
-                      });
-                    })
-                    
+
+                    message.textContent = 'Cadastro realizado com sucesso:' + user.nome;
                 })
                 .catch(error => {
                     message.textContent = 'Erro no cadastro: ' + error.message;
                 });
-        // });
+        });
 
         // Função para lidar com o login
         loginButton.addEventListener('click', () => {
             const email = loginEmail.value;
-            const password = loginPassword.value;
+            const senha = loginPassword.value;
 
-            firebase.auth().signInWithEmailAndPassword(email, password)
+            firebase.auth().signInWithEmailAndPassword(email, senha)
                 .then(userCredential => {
                     message.textContent = 'Login bem-sucedido';
                 })
---
+++ js/config.js
@@ -0,0 +1,12 @@
+// Credenciais
+const firebaseConfig = {
+    apiKey: "AIzaSyDB_oNjKWo0Z6P-SXFrlmrpQ-Cl1u-De3A",
+    authDomain: "tgfatec-4d40d.firebaseapp.com",
+    databaseURL: "https://tgfatec-4d40d-default-rtdb.firebaseio.com",
+    projectId: "tgfatec-4d40d",
+    storageBucket: "tgfatec-4d40d.appspot.com",
+    messagingSenderId: "252926034214",
+    appId: "1:252926034214:web:f07523455ac9e16ed641e7"
+}
+
+export default firebaseConfig;
